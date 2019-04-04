4c6677fe7ec7d3ad328ddd6c7179d906705bde07
Manish baghel <baghelmanish660@gmail.com>
2019-04-04 07:58:33 +0530

    updated all algos

---
+++ Sorts/bogoSort.js
@@ -0,0 +1,55 @@
+/*
+* A simple helper function that checks, if the array is
+* sorted in ascending order.
+*/
+Array.prototype.isSorted = function() {
+
+  let length = this.length;
+
+  if (length < 2) {
+    return true;
+  }
+
+  for (let i = 0; i < length - 1; i++) {
+    if (this[i] > this[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/*
+* A simple helper function to shuffle the array randomly in place.
+*/
+Array.prototype.shuffle = function() {
+
+  for (let i = this.length -1; i; i--) {
+    let m = Math.floor(Math.random() * i);
+    let n = this[i - 1];
+    this[i - 1] = this[m];
+    this[m] = n;
+  }
+
+};
+
+/*
+* Implementation of the bogosort algorithm. This sorting algorithm randomly
+* rearranges the array until it is sorted.
+* For more information see: https://en.wikipedia.org/wiki/Bogosort
+*/
+function bogoSort(items) {
+
+  while(!items.isSorted()){
+    items.shuffle()
+  }
+  return items;
+}
+
+//Implementation of bogoSort
+
+var ar = [5, 6, 7, 8, 1, 2, 12, 14];
+//Array before Sort
+console.log(ar);
+bogoSort(ar);
+//Array after sort
+console.log(ar);
---
+++ Sorts/cocktailShakerSort.js
@@ -0,0 +1,45 @@
+/*
+ * Cocktail shaker sort is a sort algorithm that is a bidirectional bubble sort
+ * more information: https://en.wikipedia.org/wiki/Cocktail_shaker_sort
+ * more information: https://en.wikipedia.org/wiki/Bubble_sort
+ *
+ */
+function cocktailShakerSort(items) {
+
+    for (let i = items.length - 1; i > 0; i--) {
+        let swapped = false;
+        let temp, j;
+
+        // backwards
+        for (j = items.length -1; j > i; j--) {
+            if (items[j] < items[j - 1]) {
+                temp = items[j];
+                items[j] = items[j - 1];
+                items[j - 1] = temp;
+                swapped = true;
+            }
+        }
+
+        //forwards
+        for (j = 0; j < i; j++) {
+            if (items[j] > items[j + 1]) {
+                temp = items[j];
+                items[j] = items[j + 1];
+                items[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            return;
+        }
+    }
+}
+
+//Implementation of cocktailShakerSort
+
+var ar = [5, 6, 7, 8, 1, 2, 12, 14];
+//Array before Sort
+console.log(ar);
+cocktailShakerSort(ar);
+//Array after sort
+console.log(ar);
